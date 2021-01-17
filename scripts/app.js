8e425312467f5503e6a7e449b19d8385a14f9726
ChrisFennek <christian.gebhardt@online.de>
2021-01-17 18:18:48 +0100

    Update

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,9 @@
 const cityForm = document.querySelector("form");
-const card = document.querySelector(".card");
+const loadingAnimation = document.querySelector(".loading-animation");
 const details = document.querySelector(".details");
-const time = document.querySelector("img.time");
-const border = document.querySelector(".form-control");
+const cityInput = document.querySelector(".change-location");
 const forecast = document.querySelector(".forecast");
+const card = document.querySelector(".card");
 
 
 
@@ -11,12 +11,15 @@ const forecast = document.querySelector(".forecast");
 const permissionSuccess = async (position) => {
   const weather = await getWeatherByPermission(position.coords.latitude, position.coords.longitude);
   const forecastData = await getForecast(position.coords.latitude, position.coords.longitude);
+  loadingAnimation.classList.add("d-none");
   displayCard(weather, forecastData);
+  cityInput.classList.remove("d-none");
 }
 
 /* User Location abgelehnt */
 const permissionDenied = () => {
-  border.classList.add("animation-border");
+  loadingAnimation.classList.add("d-none");
+  cityInput.classList.remove("d-none");
 };
 
 /* Nachfrage User Location */
@@ -25,34 +28,47 @@ window.navigator.geolocation.getCurrentPosition(permissionSuccess, permissionDen
 /* Anzeige auf der Card */
 const displayCard = (data, forecastData) => {
 
+  card.classList.add("rounded-corner");
   /* Aktuelle Wetterdaten anzeigen */
   details.innerHTML = `
     <div class="icon bg light mx-auto text-center">
-      <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="Wetter Symbol">
-       </div>
-        <h5 class="my-3">${data.name}</h5>
-        <div class="my-3">${data.weather[0].description}</div>
-        <div class="display-4 my-2">
-          <span>${Math.round(data.main.temp)}</span>
-          <span>&deg;C</span>
-        </div>
-        <div class="mt-5 mb-2">Vorhersage</div>
+    <div class="row">
+    <div class="col my-auto">
+    <div>Min</div>
+    <div>${Math.round(data.main.temp_min * 10) / 10}<span>&deg;C</span></div>
+  </div>
+  <div class="col">
+  <h5 class="mt-3 mb-0 bold text-upper">${data.name}</h5>
+  <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="Wetter Symbol">
+    <div>${data.weather[0].description}</div>
+    <div class="display-6 mb-3 bold">
+      <span>${Math.round(data.main.temp * 10) / 10}</span>
+      <span>&deg;C</span>
+    </div>
+  </div>
+  <div class="col my-auto">
+  <div>Max</div>
+  <div>${Math.round(data.main.temp_max * 10) / 10}<span>&deg;C</span></div>
+  </div>
+  </div>
+    
   `;
 
   /* Vorhersage auf die Karte setzen */
+  forecast.innerHTML = ``;
   for (let i = 1; i < 7; i++) {
 
     /* Datum konvertieren */
     const milliseconds = forecastData.daily[i].dt * 1000;
     const dateObject = new Date(milliseconds).toLocaleDateString("de-DE", {
-      day: "numeric",
-      month: "long"
+      weekday: "short",
+      /* month: "short" */
     });
 
     forecast.innerHTML += `
       <div class="col my-4">
         <div>${dateObject}</div>
-        <img src="http://openweathermap.org/img/wn/${forecastData.daily[i].weather[0].icon}@2x.png" alt="Wetter Symbol">
+        <img src="http://openweathermap.org/img/wn/${forecastData.daily[i].weather[0].icon}@2x.png" height="90px" alt="Wetter Symbol">
          <div>
          <span>${Math.round(forecastData.daily[i].temp.day)}</span>
          <span>&deg;C</span>
@@ -60,23 +76,20 @@ const displayCard = (data, forecastData) => {
     </div>
     `;
   };
-
-  /* d-none Klasse entfernen falls vorhanden */
-  if (card.classList.contains("d-none")) {
-    card.classList.remove("d-none");
-  }
 };
 
 /* Ausgelesene Stadt benutzen um Wetter abzurufen */
 const setCity = async (city) => {
   const weather = await getWeather(city);
   const forecastData = await getForecast(weather.coord.lat, weather.coord.lon);
+  loadingAnimation.classList.add("d-none");
   displayCard(weather, forecastData);
 };
 
 /* Name Stadt auslesen */
 cityForm.addEventListener("submit", e => {
   e.preventDefault();
+  loadingAnimation.classList.remove("d-none");
   const city = cityForm.city.value.trim();
   cityForm.reset();
   setCity(city);
